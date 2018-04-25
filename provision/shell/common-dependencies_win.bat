6db72f214d9ec12f0db0de1b809623abd2162a73
Lucendio <dev@lucend.io>
2018-04-25 20:20:35 +0200

    in the process of reworking win node provision

--- provision/shell/common-dependencies_win.bat
+++ provision/shell/common-dependencies_win.bat
@@ -1,9 +1,17 @@
 @echo off
 
 
+set SOURCE_PATH=%1
+set MOUNT_PATH=%2
+set CHEF_SERVER_IP=%3
+
+
 :: NOTE: required because when using the script in this context (vagrant, provisioning, batch), the
 :: cmd env does not respect the changed PATH var by chocolaty
 PATH=%PATH%;C:\ProgramData\chocolatey\bin
 
 
 choco install -y curl
+
+
+netsh advfirewall set privateprofile state off
--- provision/shell/provision_chef-node_win.ps1
+++
@@ -1,3 +0,0 @@
-$MOUNTPATH= $args[0].Trim()
-secedit.exe /configure /db C:\Windows\security\new.sdb /cfg $MOUNTPATH\assets\win\secconfig.cfg /areas SECURITYPOLICY
-powershell.exe $MOUNTPATH\shell\install-chef-user_win.ps1
